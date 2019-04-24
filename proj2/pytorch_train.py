e8791fb29f7b29b664cb77b1787be0ab9bc73125
zhaodongsun <zhaodongsun@gmail.com>
2019-04-24 16:03:18 +0200

    add comments

---
+++ proj2/pytorch_train.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from data import *
+
+train_loader = dataloader(10)
+test_loader = dataloader(10)
+
+model = nn.Sequential(nn.Linear(2, 25),
+                      nn.ReLU(),
+                      nn.Linear(25, 25),
+                      nn.ReLU(),
+                      nn.Linear(25, 25),
+                      nn.ReLU(),
+                      nn.Linear(25, 2),
+                      nn.tanh())
+optimizer = torch.optim.SGD(model.parameters(),lr=1e-1)
+
+for epoch in range(50):
+    for x,label_1d,label_2d in train_loader:
+        pred = model(x)
+        loss = ((pred-label_2d)**2).mean()
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+    acc_train = 0
+    for x, label_1d, label_2d in train_loader:
+        pred_2d = model(x)
+        pred_1d = label_2dto1d(pred_2d)
+        acc_train += (pred_1d == label_1d).float().sum()
+    acc_test = 0
+    for x,label_1d,label_2d in test_loader:
+        pred_2d = model(x)
+        pred_1d = label_2dto1d(pred_2d)
+        acc_test += (pred_1d==label_1d).float().sum()
+    print('epoch: %d/50 loss: %f train error %2.2f%% test error %2.2f%%'%(epoch, loss, (1-acc_train.item()/1000)*100, (1-acc_test.item()/1000)*100))
--- proj2/test.py
+++ proj2/test.py
@@ -3,7 +3,7 @@ import torch
 from data import *
 torch.set_grad_enabled(False)
 
-train_loader = dataloader(10)
+train_loader = dataloader(10) # my dataloader with batchsize 10
 test_loader = dataloader(10)
 
 model = Sequential(Linear(2, 25),
@@ -13,25 +13,30 @@ model = Sequential(Linear(2, 25),
                    Linear(25, 25),
                    ReLU(),
                    Linear(25, 2),
-                   tanh())
-loss_mse = LossMSE(model)
-optimizer = SGD(lr=1e-1, model=model)
+                   tanh()) # sequential model
+loss_mse = LossMSE(model) # mse loss
+optimizer = SGD(lr=1e-1, model=model) # define the optimizer
 
 for epoch in range(50):
     for x,label_1d,label_2d in train_loader:
-        pred = model(x)
+        pred = model(x) # prediction
         loss = loss_mse(pred, label_2d)
-        model.zero_grad()
-        loss_mse.backward()
-        optimizer.step()
+
+        model.zero_grad() # set all gradients to zero
+        loss_mse.backward() # backward to calculate the gradients
+        optimizer.step() # update parameters with the gradients
+
+    #evaluation on training data
     acc_train = 0
     for x, label_1d, label_2d in train_loader:
         pred_2d = model(x)
-        pred_1d = label_2dto1d(pred_2d)
+        pred_1d = label_2dto1d(pred_2d) # convert 2d label to 1d label (0 and 1)
         acc_train += (pred_1d == label_1d).float().sum()
+
+    #evaluation on test data
     acc_test = 0
     for x,label_1d,label_2d in test_loader:
         pred_2d = model(x)
-        pred_1d = label_2dto1d(pred_2d)
+        pred_1d = label_2dto1d(pred_2d) # convert 2d label to 1d label (0 and 1)
         acc_test += (pred_1d==label_1d).float().sum()
-    print('epoch: %d loss: %f train error %2.2f%% test error %2.2f%%'%(epoch, loss, (1-acc_train.item()/1000)*100, (1-acc_test.item()/1000)*100))
+    print('epoch: %d/50 loss: %f train error %2.2f%% test error %2.2f%%'%(epoch, loss, (1-acc_train.item()/1000)*100, (1-acc_test.item()/1000)*100))
